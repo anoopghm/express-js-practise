a71840f2664961397ae8aaecf6377019a06e783e
anoopghm <140532297+anoopghm@users.noreply.github.com>
2024-02-23 00:27:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
-//The password is ILoveProgramming
+//The password is ILoveAnisha
 import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
